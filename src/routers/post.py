45f8a66c04bd81b4d09e929e68cd707c03604a2b
H. Hills <humphrey.hills@gmail.com>
2023-08-19 08:25:57 -0500

    Fixed issue with line 74 in get post.py get_post by id

--- src/routers/post.py
+++ src/routers/post.py
@@ -71,7 +71,7 @@ def get_post(id: int,
     # post = db.query(models.Post).filter(models.Post.id == id).first()
 
     post = db.query(models.Post, func.count(models.Votes.post_id).label("votes")).join(
-        models.Votes, models.Votes.post_id == models.Post.id, isouter=True).group_by(models.Post.id == id).first()
+        models.Votes, models.Votes.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.id == id).first()
     
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
